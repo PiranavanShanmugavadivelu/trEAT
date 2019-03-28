b42dc0a31ca0daa2d72608fb3aa95aeddc5ab077
Piranavan Shanmugavadivelu <shanpirana@gmail.com>
2019-03-28 11:31:00 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import RootStack from "./route";
+
+class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
+export default App;
---
+++ route.js
@@ -0,0 +1,63 @@
+// import React from 'react';
+// import { Text, View } from 'react-native';
+// import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+// import Icon from "react-native-vector-icons";
+
+// class Home extends React.Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//         <Text>Home!</Text>
+//       </View>
+//     );
+//   }
+// }
+
+
+// const RootStack = createBottomTabNavigator({
+//     Home: {
+//       screen: Home,
+//       navigationOptions: {
+//         tabBarLabel: "Home",
+//         tabBarIcon: ({ tintColor, focused }) => (
+//           <Icon
+//             name={"comment"}
+//             size={26}
+
+//           />
+//         )
+//       }
+//     }
+// });
+
+// export default createAppContainer(RootStack);
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createBottomTabNavigator({
+  Home: { screen: HomeScreen },
+  Settings: { screen: SettingsScreen },
+});
+
+export default createAppContainer(RootStack);
